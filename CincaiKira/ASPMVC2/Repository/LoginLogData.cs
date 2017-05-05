927a472874ac2f2771dd28a7b829d5381c09d93b
cKerel <cKerel@CKEREL-PC>
2017-05-06 07:54:49 +0800

    added database

---
+++ CincaiKira/ASPMVC2/Repository/LoginLogData.cs
@@ -0,0 +1,12 @@
+﻿using ASPMVC2.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVC2.Repository
+{
+    public class LoginLogData : YugimotoDataContext
+    {
+    }
+}
